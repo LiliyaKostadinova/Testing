d5ec7b492a4437d145f4ccd8528ae2e2474c4088
LiliyaKostadinova <92646484+LiliyaKostadinova@users.noreply.github.com>
2021-12-24 15:19:26 +0200

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+//namirane na sbor na cifri na chislo
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int num = 0, currDigit = 0, sum = 0;
+    cin >> num;
+
+    while(num > 0){
+        currDigit = num % 10;
+        sum = sum + currDigit;
+        num = num/10;
+    }
+    cout<<sum;
+
+
+    return 0;
+}
